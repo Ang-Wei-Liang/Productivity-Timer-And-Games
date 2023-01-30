d2d30d3dd0c2857c5534803cfb486b8381cdb917
Ang-Wei-Liang <113972542+Ang-Wei-Liang@users.noreply.github.com>
2023-01-30 07:44:37 +0000

    30/1/344

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 //import { useAuth } from './auth';
 
-import AsyncStorage from '@react-native-community/async-storage'
+
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { StyleSheet, Text, View } from 'react-native';
